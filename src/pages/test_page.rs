d6813d5399358901cf07771f8796c8f18f2ae01c
Wensen Zhang <wensen.zhg@gmail.com>
2024-04-02 13:44:54 -0600

    Update style to be closer to the design

--- src/pages/test_page.rs
+++ src/pages/test_page.rs
@@ -5,44 +5,43 @@ use leptos_meta::Stylesheet;
 pub fn TestPage() -> impl IntoView {
     
     view!{
-        <Stylesheet id="leptos" href="/pkg/encampus.css"/>
-
-        <div class="bg-red-600 p-4 flex justify-between items-center text-white">
-            <div class="flex items-center">
-                <img src="logo.png" alt="ENCAMPUS" class="h-8 mr-2"/>
-                <span class="text-lg font-semibold">"ENCAMPUS"</span>
+        <div class="bg-gray-200 min-h-screen">
+            <Stylesheet id="leptos" href="/pkg/encampus.css"/>
+            // ToDo: Cleanup, extract Tailwind to reduce styling in code
+            <div class="bg-white p-4 flex justify-between items-center text-gray-600">
+                <div class="flex items-center">
+                    <img src="logo.png" alt="ENCAMPUS" class="h-8 mr-2"/>
+                    <span class="text-xl font-bold">"ENCAMPUS"</span>
+                </div>
+                <div class="relative">
+                    <input type="text" placeholder="Ask me anything..." class="p-2 rounded-full border border-gray-300 w-64"/>
+                    <button class="absolute right-0 top-0 mr-2 text-black">
+                        <i class="fas fa-search"/>
+                    </button>
+                </div>
+                <div class="flex items-center">
+                    <span class="text-xl font-bold mr-4">"LONGNAME"</span> // Todo: Replace with vh/vw for dynamic size
+                    <div class="p-2">"☰"</div>
+                </div>
             </div>
-            <div class="relative">
-                <input type="text" placeholder="Ask me anything..." class="p-2 rounded"/>
-                <button class="absolute right-0 top-0 mr-2 text-black">
-                    <i class="fas fa-search"/>
-                </button>
-            </div>
-            <div class="flex items-center">
-                <span class="mr-4">"LONGNAME"</span>
-                <div class="p-2">"☰"</div>
-            </div>
-        </div>
-        // TODO: Dynamically generate tiles
-        <div class="flex flex-wrap justify-center gap-4 p-4">
-            <div class="tile bg-white rounded shadow p-4 w-32 h-24 flex items-center justify-center font-bold">
+            // ToDo: Dynamically generate tiles
+            <div class="grid grid-cols-3 gap-4 p-10 mx-20">
+            <div class="tile bg-white rounded shadow p-4 flex items-center justify-center font-bold h-32">
                 "Math 3210"
             </div>
-            <div class="tile bg-white rounded shadow p-4 w-32 h-24 flex items-center justify-center font-bold">
+            <div class="tile bg-white rounded shadow p-4 flex items-center justify-center font-bold h-32">
                 "Class 3124"
             </div>
-            <div class="tile bg-white rounded shadow p-4 w-32 h-24 flex items-center justify-center font-bold">
+            <div class="tile bg-white rounded shadow p-4 flex items-center justify-center font-bold h-32">
                 "Class 4123"
             </div>
-            <div class="tile bg-white rounded shadow p-4 w-32 h-24 flex items-center justify-center font-bold">
+            <div class="tile bg-white rounded shadow p-4 flex items-center justify-center font-bold h-32">
                 "Class 3214"
             </div>
-            <div class="tile bg-white rounded shadow p-4 w-32 h-24 flex items-center justify-center font-bold">
+            <div class="tile bg-white rounded shadow p-4 flex items-center justify-center font-bold h-32">
                 "Class 1243"
             </div>
-            <div class="tile bg-white rounded shadow p-4 w-32 h-24 flex items-center justify-center font-bold">
-                "Class 2341"
-            </div>
+        </div>
         </div>
     }
 }
