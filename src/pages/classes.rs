365e68f20bd44f4f99d76533772636159cae1c10
matthew <m4ttm4n.t4yl0r@gmail.com>
2024-04-16 14:21:15 -0600

    Added database queries to get class list

--- src/pages/classes.rs
+++ src/pages/classes.rs
@@ -1,73 +1,82 @@
 use leptos::{
-    component, create_resource, server, view, For, IntoView, ServerFnError, SignalSet, Suspense,
+    component, create_resource, server, view, CollectView, For, IntoView, ServerFnError, SignalSet,
+    Suspense,
 };
 //use leptos::{component, view, For, IntoView};
 
-use crate::components::class_tile::ClassTile;
+//use crate::components::class_tile::ClassTile;
 use crate::components::header::Header;
 
 #[component]
-fn ClassCard() -> impl IntoView {
-    let class_name = create_resource(
-        || {},
-        |_| async move {
-            let _class_names = get_class_name()
-                .await
-                .unwrap_or_else(|_err| vec!["Failed".to_string()]);
-        },
-    );
-
+pub fn ClassTile(class_id: String) -> impl IntoView {
     view! {
-        <div class="bg-white rounded shadow p-4 flex flex-col items-center justify-center font-bold h-32">
-            <Suspense
-                fallback=move || view! { <p>"Loading..."</p> }
-            >
-                {move || class_name().unwrap_or_default()}
-            </Suspense>
-            // <button class="border text-sm" on:click=move |_| { class_name.set("Loading...".to_string()); class_name.refetch(); }>
-            //     "Click Me To Send Request to Server (watch the network requests)"
-            // </button>
+        <a href=&format!("class/{}", class_id.replace(" ", ""))>
+        <div class="tile bg-white rounded shadow p-4 flex items-center justify-center font-bold h-32">
+            {class_id} //TODO: get title from DB
         </div>
+        </a>
     }
 }
+
+// #[component]
+// fn ClassCard() -> impl IntoView {
+//     let class_names = create_resource(
+//         || {},
+//         |_| async move { get_class_name().await.unwrap_or(vec!["Failed".to_string()]) },
+//     );
+
+//     let classes_list = class_names()
+//         .unwrap_or_default()
+//         .into_iter()
+//         .map(|class| {
+//             view! {
+//                     <ClassTile class_id=class.clone()/>
+//             }
+//         })
+//         .collect_view();
+// }
 
 /**
  * Page showing all classes registered to the user
  */
 #[component]
 pub fn ClassesPage() -> impl IntoView {
-    //TODO: Load this based on user data
-    // let class_ids = vec![
-    //     "Math3210".to_string(),
-    //     "Class3124".to_string(),
-    //     "Class4123".to_string(),
-    //     "Class3214".to_string(),
-    //     "Class1243".to_string(),
-    // ];
-    let class_ids = match get_class_name().await {
-        Ok(names) => names,
-        Err(err) => {
-            vec!["Failed".to_string()]
-        }
-    };
+    let class_names = create_resource(
+        || {},
+        |_| async move { get_class_name().await.unwrap_or(vec!["Failed".to_string()]) },
+    );
+
+    // let classes_list = class_names()
+    //     .unwrap_or_default()
+    //     .into_iter()
+    //     .map(|class| {
+    //         view! {
+    //             {println!("class names: {:?}", class_names());}
+    //             <ClassTile class_id=class.clone()/>
+    //         }
+    //     })
+    //     .collect_view();
 
     view! {
         <Header text="ENCAMPUS".to_string() logo="logo.png".to_string() />
 
         <div class="grid grid-cols-3 gap-4 p-10 mx-20">
-            <For each=class_ids.iter().cloned() key=|class_id| class_id.clone()>
+            <For each=move || class_names().unwrap_or_default().clone() key=|id| id.clone() let:class_id>
                 <ClassTile class_id={class_id} />
             </For>
-            // <For each=move || class_ids.clone() key=|id| id.clone() let:class_id>
-            //     <ClassTile class_id={class_id} />
-            // </For>
+
+           //{classes_list}
         </div>
     }
 }
 
-// struct Classname {
-//     name: String,
-// }
+#[cfg(feature = "ssr")]
+#[derive(sqlx::FromRow)]
+struct Classname(String);
+
+#[cfg(feature = "ssr")]
+#[derive(sqlx::FromRow)]
+struct ClassNumPosts(i32);
 
 #[server(GetClassName)]
 async fn get_class_name() -> Result<Vec<String>, ServerFnError> {
@@ -78,11 +87,27 @@ async fn get_class_name() -> Result<Vec<String>, ServerFnError> {
         "Unable to complete Request".to_string(),
     ))?;
 
-    let row: Vec<(i32, String, String)> = sqlx::query_as("SELECT * from classes LIMIT 1")
+    let rows: Vec<Classname> = sqlx::query_as("SELECT coursename from classes")
         .fetch_all(&pool)
         .await
         .expect("select should work");
 
-    let names: Vec<String> = row.into_iter().map(|(_, s1, _)| s1).collect();
+    let names: Vec<String> = rows.into_iter().map(|row| row.0).collect();
     Ok(names)
 }
+
+// #[server(GetNumClasses)]
+// async fn get_num_classes() -> Result<i32, ServerFnError> {
+//     use leptos::{server_fn::error::NoCustomError, use_context};
+//     use sqlx::postgres::PgPool;
+
+//     let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+//         "Unable to complete Request".to_string(),
+//     ))?;
+
+//     let ClassNumPosts(num_posts) = sqlx::query_as("SELECT COUNT(*) FROM classes;")
+//         .fetch_one(&pool)
+//         .await
+//         .expect("select should work");
+//     Ok(num_posts)
+// }
