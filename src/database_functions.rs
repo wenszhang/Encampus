dda870d3b4d06a6544e8331bbd691e80121f4cc7
m4ttm4n <m4ttm4n.t4yl0r@gmail.com>
2024-04-17 22:12:30 -0600

    Added database functions file

---
+++ src/database_functions.rs
@@ -0,0 +1,79 @@
+use leptos::{server, ServerFnError};
+use serde::{Deserialize, Serialize};
+
+/**
+ * Struct to hold the class info
+ */
+
+#[derive(Clone, Serialize, Deserialize)]
+#[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
+pub struct ClassInfo {
+    pub id: i32,
+    pub name: String,
+}
+
+/**
+ * Get all class names from the database
+ * Will eventually have a user added and so query will be modified to get only the classes the user is registered to
+ */
+#[server(GetClassList)]
+pub async fn get_class_list() -> Result<Vec<ClassInfo>, ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::postgres::PgPool;
+
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete Request".to_string(),
+    ))?;
+
+    let classes: Vec<ClassInfo> =
+        sqlx::query_as("SELECT courseid as id, coursename as name from classes")
+            .fetch_all(&pool)
+            .await
+            .expect("select should work");
+
+    Ok(classes)
+}
+
+#[cfg(feature = "ssr")]
+#[derive(sqlx::FromRow)]
+pub struct Post(String);
+
+#[cfg(feature = "ssr")]
+#[derive(sqlx::FromRow)]
+pub struct ClassName(String);
+
+#[server(GetPosts)]
+pub async fn get_posts(class_id: i32) -> Result<Vec<String>, ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::postgres::PgPool;
+
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete Request".to_string(),
+    ))?;
+
+    let rows: Vec<Post> = sqlx::query_as( "select title from posts join classes on posts.classid = classes.courseid where classes.courseid = $1")
+        .bind(class_id)
+        .fetch_all(&pool)
+        .await
+        .expect("select should work");
+
+    let post_titles: Vec<String> = rows.into_iter().map(|row| row.0).collect();
+    Ok(post_titles)
+}
+
+#[server(GetClassName)]
+pub async fn get_class_name(class_id: i32) -> Result<String, ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::postgres::PgPool;
+
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete Request".to_string(),
+    ))?;
+
+    let ClassName(name) = sqlx::query_as("select coursename from classes where courseid = $1")
+        .bind(class_id)
+        .fetch_one(&pool)
+        .await
+        .expect("select should work");
+    Ok(name)
+}
