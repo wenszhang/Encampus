4fa524bee5587b82fd30f942f0558b07dcb78cee
Wensen Zhang <wensen.zhg@gmail.com>
2024-04-22 14:08:04 -0600

    Add header

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,14 +1,28 @@
+use crate::components::header::Header;
 use leptos::{component, view, IntoView};
 use leptos_router::A;
 
-/// Renders the home page of your application.
+/// Renders the home page
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
-        <A href="/dev">"Dev Page"</A>
-        <br/>
-        <A href="/classes">"Classes Page"</A>
-        <A href="/login">"Login Page"</A>
-        <br/>
+        <Header text="ENCAMPUS".to_string() logo="logo.png".to_string() />
+        <div class="container mx-auto px-4">
+            <header class="flex justify-between items-center py-4">
+                <h1 class="text-4xl font-bold">"Welcome to Encampus"</h1>
+            </header>
+            <main class="my-8">
+                <p class="text-xl mb-4">"Supper snazzy tagline to draw your interest"</p>
+                <A
+                    href="/login"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    "Login to your account"
+                </A>
+            </main>
+            <footer class="py-4">
+                <p>"© 2024 Encampus"</p>
+            </footer>
+        </div>
     }
 }
