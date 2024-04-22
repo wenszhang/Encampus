705d9cea01d605ae416a34bebb79176deafa413e
Wensen Zhang <wensen.zhg@gmail.com>
2024-04-21 19:03:08 -0600

    Simplify logic for static pages

--- src/components/page.rs
+++ src/components/page.rs
@@ -1,17 +1,11 @@
-use leptos::{component, create_effect, leptos_dom, on_cleanup, view, IntoView};
+use leptos::{component, view, IntoView};
+use leptos_meta::Title;
 use leptos_router::Outlet;
 
 #[component]
 pub fn Page() -> impl IntoView {
-    create_effect(move |_| {
-        leptos_dom::document().set_title("Encpamus");
-        // Cleanup function (in case we ever need it)
-        on_cleanup(move || {
-            leptos_dom::document().set_title("Encpamus");
-        });
-    });
-
     view! {
+        <Title text="Encapmus"/>
         <div class="bg-gray-200 min-h-screen">
             <Outlet/>
         </div>
