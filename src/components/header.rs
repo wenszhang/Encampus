c42226953512d69c4fca6febf6c47cdbd281b21e
Wentao Zhang <u1334599@utah.edu>
2024-04-22 16:32:52 -0600

    fix linting

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,6 +1,6 @@
+use crate::svgs::announcement_bell::AnnouncementBell;
 use crate::svgs::drop_down_bars::DropDownBars;
 use crate::svgs::magnifying_glass::MagnifyingGlass;
-use crate::svgs::announcement_bell::AnnouncementBell;
 use leptos::*;
 
 #[component]
