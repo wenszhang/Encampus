6000ca3cec13f79679803c79a92bca51a6ed26a4
m4ttm4n <m4ttm4n.t4yl0r@gmail.com>
2024-08-08 23:21:13 -0600

    modified colors of unanswered tiles

--- src/pages/view_class_posts/question_tile.rs
+++ src/pages/view_class_posts/question_tile.rs
@@ -20,7 +20,7 @@ pub fn QuestionTile(post: Post) -> impl IntoView {
 pub fn UnansweredQuestionTile(post: Post) -> impl IntoView {
     view! {
         <A href=format!("{}", post.post_id)>
-            <div class="tile bg-white rounded shadow p-4 flex items-center justify-center font-bold h-32 transition duration-300 hover:bg-red-600">
+            <div class="tile bg-red-500 rounded shadow p-4 flex items-center justify-center font-bold h-32 transition duration-300 hover:bg-red-700">
                 {post.title}
             </div>
         </A>
