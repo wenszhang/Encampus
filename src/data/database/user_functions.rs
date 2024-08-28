20d359a74f085680d43ccdf2af2b0c8fbdcd5e1e
Matthew <m4ttm4n.t4yl0r@gmail.com>
2024-08-28 12:33:00 -0600

    Added a user function to get a user with an id and setup user functions file to have server functions:

--- src/data/database/user_functions.rs
+++ src/data/database/user_functions.rs
@@ -1,11 +1,36 @@
+use leptos::{server, ServerFnError};
+use serde::{Deserialize, Serialize};
+
 /**
  * Struct to hold user
  */
+#[derive(Clone, Serialize, Deserialize)]
 #[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
 pub struct User {
-    pub name: String,
+    pub username: String,
+    pub firstname: String,
+    pub lastname: String,
     pub id: i32,
 }
 
 #[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
 pub struct UserId(pub i32);
+
+#[server(GetUserInfo)]
+pub async fn get_user_info(id: i32) -> Result<User, ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::postgres::PgPool;
+
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete Request".to_string(),
+    ))?;
+
+    let current_user: User =
+        sqlx::query_as("select username, firstname, lastname, id from users where id = $1")
+            .bind(id)
+            .fetch_one(&pool)
+            .await
+            .expect("failed getting user");
+
+    Ok(current_user)
+}
