d2836a9ed99b113d34a3b4df0296ae7f3033b0cf
Matthew <m4ttm4n.t4yl0r@gmail.com>
2024-09-25 10:45:01 -0600

    Removed one more thing that wasn't needed

--- src/pages/view_class_posts/class.rs
+++ src/pages/view_class_posts/class.rs
@@ -16,7 +16,6 @@ use crate::resources::images::svgs::magnifying_glass::MagnifyingGlass;
 
 use leptos::*;
 use leptos_router::{use_params, Outlet, Params};
-use serde::de;
 
 #[derive(Params, PartialEq, Clone)]
 pub struct ClassId {
