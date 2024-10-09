8512637d4686068289fe0a1cf6d4b448a7a4ec4d
m4ttm4n <m4ttm4n.t4yl0r@gmail.com>
2024-10-08 21:31:07 -0600

    Implemented the adding a new user section

--- src/pages/admin_pages/admin_homepage.rs
+++ src/pages/admin_pages/admin_homepage.rs
@@ -1,5 +1,7 @@
 use crate::data::database::class_functions::get_class_list;
 use crate::data::database::user_functions::get_users;
+use crate::data::database::user_functions::User;
+use crate::data::database::user_functions::add_user;
 use crate::pages::global_components::header::Header;
 use leptos::*;
 use leptos::{component, create_resource, view, For, IntoView, Signal};
@@ -13,10 +15,16 @@ pub fn AdminHomePage() -> impl IntoView {
         |_| async { get_class_list().await.unwrap_or_default() },
     );
 
-    let (is_visible, set_is_visible) = create_signal(false);
+    let (new_user_visible, set_new_user_visible) = create_signal(false);
 
     view! {
       <Header text="ENCAMPUS".to_string() logo=None class_id=Signal::derive(|| None) />
+      <div class="mt-6 mx-6 space-x-4">
+        <Show when=move || new_user_visible.get() fallback=|| ()>
+          <AddUserOptions/>
+        </Show>
+      </div>
+      
       <div class="flex mx-6 mt-6 space-x-4">
         <div class="w-1/2">
           <div class="p-6 bg-white rounded-lg shadow-md">
@@ -24,7 +32,7 @@ pub fn AdminHomePage() -> impl IntoView {
               <h2 class="mb-4 text-lg font-semibold">"Users"</h2>
               <button
                 class="py-1 px-2 text-white rounded-full focus:ring-2 focus:ring-offset-2 focus:outline-none bg-customBlue hover:bg-customBlue-HOVER focus:ring-offset-customBlue"
-                on:click=move |_| set_is_visible(!is_visible())
+                on:click=move |_| set_new_user_visible(!new_user_visible())
               >
                 "Create User"
               </button>
@@ -48,14 +56,13 @@ pub fn AdminHomePage() -> impl IntoView {
             </div>
           </div>
         </div>
-
         <div class="w-1/2">
           <div class="p-6 bg-white rounded-lg shadow-md">
             <div class="flex justify-between items-center">
               <h2 class="mb-4 text-lg font-semibold">"Open Classes"</h2>
               <button
                 class="py-1 px-2 text-white rounded-full focus:ring-2 focus:ring-offset-2 focus:outline-none bg-customBlue hover:bg-customBlue-HOVER focus:ring-offset-customBlue"
-                on:click=move |_| set_is_visible(!is_visible())
+                // on:click=move |_| set_is_visible(!is_visible())
               >
                 "Create Class"
               </button>
@@ -87,5 +94,86 @@ pub fn AdminHomePage() -> impl IntoView {
 
 #[component]
 fn AddUserOptions() -> impl IntoView {
-    view! { <div class="p-6 bg-white rounded-lg shadow-md"></div> }
+    let (first_name, set_first_name) = create_signal("".to_string());
+    let (last_name, set_last_name) = create_signal("".to_string());
+    let (username, set_username) = create_signal("".to_string());
+    let (role, set_role) = create_signal("Student".to_string());
+
+    let on_input = |setter: WriteSignal<String>| {
+      move |ev| {
+          setter(event_target_value(&ev));
+      }
+  };
+
+    let add_user_action = create_action(move |user: &User| {
+      let user = user.clone();
+      async move {
+        add_user(user).await.unwrap();
+      }
+    });
+    view! { 
+      <div class="p-6 bg-white rounded-lg shadow-md">
+        <h2 class="mb-4 text-lg font-semibold">"New User"</h2>
+        <div class="grid grid-cols-1 gap-4">
+          <div>
+            <label class="block text-sm font-medium text-gray-700">"First Name"</label>
+            <input
+              type="text"
+              class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              on:input=on_input(set_first_name)
+              prop:value=first_name
+            />
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-700">"Last Name"</label>
+            <input
+              type="text"
+              class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              on:input=on_input(set_last_name)  
+              prop:value=last_name
+            />
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-700">"Username"</label>
+            <input
+              type="text"
+              class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              on:input=on_input(set_username) 
+              prop:value=username
+            />
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-700">"Role"</label>
+            <select
+              class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              on:change=on_input(set_role)
+              prop:value=role
+            >
+              <option>"Student"</option>
+              <option>"Instructor"</option>
+              <option>"Admin"</option>
+            </select>
+          </div>
+        </div>
+        <div class="mt-4 text-right">
+          <button
+            class="py-1 px-2 text-white rounded-full focus:ring-2 focus:ring-offset-2 focus:outline-none bg-customBlue hover:bg-customBlue-HOVER focus:ring-offset-customBlue"
+            on:click=move |_| {add_user_action.dispatch(User {
+              username: username.get(),
+              firstname: first_name.get(),
+              lastname: last_name.get(),
+              role: role.get(),
+              id: 0, // Will get overwritten when inserted, but required for struct
+            });
+            set_first_name("".to_string());
+            set_last_name("".to_string());
+            set_username("".to_string());
+            set_role("Student".to_string());
+          }
+          >
+            "Submit"
+          </button>
+        </div>
+      </div>
+    }
 }
