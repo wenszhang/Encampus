7f316f905c13c39d19b5d802fa3b72c6fb743f74
Wensen Zhang <wensen.zhg@gmail.com>
2024-10-17 17:40:01 -0600

    Load classes in sidebar based on user instead of role

--- src/pages/global_components/sidebar.rs
+++ src/pages/global_components/sidebar.rs
@@ -1,4 +1,5 @@
 use crate::data::database::class_functions::get_class_list;
+use crate::data::database::class_functions::get_students_classes;
 use crate::data::database::class_functions::ClassInfo;
 use crate::data::global_state::GlobalState;
 use leptos::*;
@@ -6,27 +7,29 @@ use leptos_router::A;
 
 #[component]
 pub fn Sidebar() -> impl IntoView {
+    let global_state = expect_context::<GlobalState>(); // Access global state
     let (collapsed, set_collapsed) = create_signal(false);
 
     let courses = create_resource(
         || {},
-        |_| async { get_class_list().await.unwrap_or_default() },
+        move |_| {
+            let id = global_state.id.get().unwrap_or_default();
+            async move { get_students_classes(id).await.unwrap_or_default() }
+        },
     );
 
+    let class = if collapsed.get() {
+        "sticky top-0 h-screen w-8 bg-gray-800 text-white flex items-center justify-center"
+    } else {
+        "sticky top-0 h-screen w-64 bg-gray-800 text-white"
+    };
+
     view! {
-      <div class=move || {
-        if collapsed.get() {
-          "sticky top-0 h-screen w-8 bg-gray-800 text-white flex items-center justify-center"
+      <div class=class>
+        {if collapsed.get() {
+          collapsed_view(set_collapsed).into_view()
         } else {
-          "sticky top-0 h-screen w-64 bg-gray-800 text-white"
-        }
-      }>
-        {move || {
-          if collapsed() {
-            collapsed_view(set_collapsed).into_view()
-          } else {
-            expanded_view(set_collapsed, courses).into_view()
-          }
+          expanded_view(set_collapsed, courses, global_state).into_view()
         }}
       </div>
     }
@@ -43,9 +46,11 @@ fn collapsed_view(set_collapsed: WriteSignal<bool>) -> View {
 }
 
 // Expanded view for the sidebar
-fn expanded_view(set_collapsed: WriteSignal<bool>, courses: Resource<(), Vec<ClassInfo>>) -> View {
-    let global_state = expect_context::<GlobalState>(); // Access global state
-
+fn expanded_view(
+    set_collapsed: WriteSignal<bool>,
+    courses: Resource<(), Vec<ClassInfo>>,
+    global_state: GlobalState,
+) -> View {
     view! {
       <div class="flex flex-col h-full">
         // Profile Image and User Info
@@ -91,29 +96,29 @@ fn expanded_view(set_collapsed: WriteSignal<bool>, courses: Resource<(), Vec<Cla
             </Suspense>
           </ul>
 
-          <h2 class="mt-6 mb-2 text-sm tracking-widest text-gray-400 uppercase">"Tools"</h2>
-          <ul>
-            <li class="py-2">
-              <A href="/classes" class="block py-2 px-4 text-white rounded-md hover:bg-gray-700">
-                "Private Messages"
-              </A>
-            </li>
-            <li class="py-2">
-              <A href="/classes" class="block py-2 px-4 text-white rounded-md hover:bg-gray-700">
-                "Course Statistics"
-              </A>
-            </li>
-            <li class="py-2">
-              <A href="/classes" class="block py-2 px-4 text-white rounded-md hover:bg-gray-700">
-                "PollV"
-              </A>
-            </li>
-          </ul>
+        // <h2 class="mt-6 mb-2 text-sm tracking-widest text-gray-400 uppercase">"Tools"</h2>
+        // <ul>
+        // <li class="py-2">
+        // <A href="/classes" class="block py-2 px-4 text-white rounded-md hover:bg-gray-700">
+        // "Private Messages"
+        // </A>
+        // </li>
+        // <li class="py-2">
+        // <A href="/classes" class="block py-2 px-4 text-white rounded-md hover:bg-gray-700">
+        // "Course Statistics"
+        // </A>
+        // </li>
+        // <li class="py-2">
+        // <A href="/classes" class="block py-2 px-4 text-white rounded-md hover:bg-gray-700">
+        // "PollV"
+        // </A>
+        // </li>
+        // </ul>
         </div>
 
         // Account Settings Button
         <div class="py-2 px-2 w-full bg-gray-700 rounded-md hover:bg-gray-600">
-          <A href="/classes" class="block py-1 w-full text-sm text-center text-white">
+          <A href="/settings" class="block py-1 w-full text-sm text-center text-white">
             "Account Settings"
           </A>
         </div>
