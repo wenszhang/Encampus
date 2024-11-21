0055aedd3712f0ddd848f829470f712235290e92
gabrielfamodu <gabefamodu13@gmail.com>
2024-11-21 00:18:48 -0700

    Live poll more progress

--- src/data/database/live_poll_functions.rs
+++ src/data/database/live_poll_functions.rs
@@ -10,6 +10,22 @@ pub struct Poll {
     pub course_id: i32,
 }
 
+#[derive(Clone, Serialize, Deserialize, Default, Debug)]
+#[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
+pub struct Answer {
+    pub pollid: i32,
+    pub answer: String,
+    pub voted_count: i32,
+}
+
+#[derive(Clone, Serialize, Deserialize, Default, Debug)]
+#[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
+pub struct StudentAnswer {
+    pub user_id: i32,
+    pub pollid: i32,
+    pub answer: String,
+}
+
 /**
  * Struct to hold the poll info
  */
@@ -35,8 +51,8 @@ pub struct PollOption {
 pub async fn create_poll(
     question: String,
     course_id: i32,
-    options: Vec<String>,
-) -> Result<PollInfo, ServerFnError> {
+    answers: Vec<String>,
+) -> Result<Poll, ServerFnError> {
     use leptos::{server_fn::error::NoCustomError, use_context};
     use sqlx::postgres::PgPool;
 
@@ -44,38 +60,148 @@ pub async fn create_poll(
         "Unable to complete request".to_string(),
     ))?;
 
-    let PollId(poll_id) = sqlx::query_as(
-        "insert into polls (question, is_active, course_id) 
-         values ($1, $2, $3) 
-         returning id",
+    // Insert poll
+    let poll_id = sqlx::query_scalar::<_, i32>(
+        "INSERT INTO polls (question, is_active, course_id)
+         VALUES ($1, $2, $3)
+         RETURNING id",
     )
-    .bind(question)
+    .bind(&question)
     .bind(true)
     .bind(course_id)
     .fetch_one(&pool)
     .await
-    .map_err(|_| {
-        ServerFnError::<NoCustomError>::ServerError("Unable to create poll".to_string())
-    })?;
+    .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
 
-    // TODO: https://github.com/launchbadge/sqlx/blob/main/FAQ.md#how-can-i-bind-an-array-to-a-values-clause-how-can-i-do-bulk-inserts USE THIS TO ADD MULTIPLE
-    let poll_options: Vec<PollOption> = sqlx::query_as(
-        "insert into poll_options (poll_id, option_text) 
-         select $1, * from unnest($2::text[]) 
-         returning id, option_text",
+    // Insert answers
+    for answer_text in answers {
+        sqlx::query(
+            "INSERT INTO answers (pollid, answer, voted_count)
+             VALUES ($1, $2, $3)",
+        )
+        .bind(poll_id)
+        .bind(&answer_text)
+        .bind(0)
+        .execute(&pool)
+        .await
+        .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+    }
+
+    // Fetch the created poll
+    let poll: Poll = sqlx::query_as("SELECT * FROM polls WHERE id = $1")
+        .bind(poll_id)
+        .fetch_one(&pool)
+        .await
+        .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+
+    Ok(poll)
+}
+
+#[server(GetPollAnswers)]
+pub async fn get_poll_answers(poll_id: i32) -> Result<Vec<Answer>, ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::postgres::PgPool;
+
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete request".to_string(),
+    ))?;
+
+    let answers: Vec<Answer> = sqlx::query_as::<_, Answer>(
+        "SELECT pollid, answer, voted_count FROM answers WHERE pollid = $1",
     )
     .bind(poll_id)
-    .bind(options)
     .fetch_all(&pool)
     .await
-    .map_err(|_| {
-        ServerFnError::<NoCustomError>::ServerError("Unable to create poll".to_string())
-    })?;
+    .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
 
-    Ok(PollInfo {
-        poll_id,
-        poll_options,
-    })
+    Ok(answers)
+}
+
+#[server(VoteOnPollAnswer)]
+pub async fn vote_on_poll_answer(
+    user_id: i32,
+    poll_id: i32,
+    new_answer: String,
+    old_answer: Option<String>,
+) -> Result<(), ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::{postgres::PgPool, Executor}; // Ensure Executor is imported
+
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete Request".to_string(),
+    ))?;
+
+    let mut tx = pool
+        .begin()
+        .await
+        .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+
+    // Borrow old_answer to avoid moving it
+    if let Some(ref old_answer_value) = old_answer {
+        sqlx::query(
+            "UPDATE answers SET voted_count = voted_count - 1
+             WHERE pollid = $1 AND answer = $2",
+        )
+        .bind(poll_id)
+        .bind(old_answer_value)
+        .execute(&mut *tx)
+        .await
+        .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+    }
+
+    // Now old_answer can still be used here
+    if old_answer.is_some() {
+        sqlx::query(
+            "UPDATE student_answers SET answer = $1
+             WHERE user_id = $2 AND pollid = $3",
+        )
+        .bind(&new_answer)
+        .bind(user_id)
+        .bind(poll_id)
+        .execute(&mut *tx)
+        .await
+        .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+    } else {
+        sqlx::query(
+            "INSERT INTO student_answers (user_id, pollid, answer)
+             VALUES ($1, $2, $3)",
+        )
+        .bind(user_id)
+        .bind(poll_id)
+        .bind(&new_answer)
+        .execute(&mut *tx)
+        .await
+        .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+    }
+
+    tx.commit()
+        .await
+        .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+
+    Ok(())
+}
+
+#[server(GetStudentAnswer)]
+pub async fn get_student_answer(
+    user_id: i32,
+    poll_id: i32,
+) -> Result<Option<String>, ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::postgres::PgPool;
+
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete request".to_string(),
+    ))?;
+
+    let result: Option<(String,)> =
+        sqlx::query_as("SELECT answer FROM student_answers WHERE user_id = $1 AND pollid = $2")
+            .bind(user_id)
+            .bind(poll_id)
+            .fetch_optional(&pool)
+            .await
+            .map_err(|e| ServerFnError::<NoCustomError>::ServerError(e.to_string()))?;
+
+    Ok(result.map(|(answer,)| answer))
 }
 
 // For student to get the options of the poll. ALso need them to get the question of the poll. get question of the poll and options of the poll in one fucntion.
@@ -120,7 +246,24 @@ pub async fn vote_on_poll_option(poll_option_id: i32) -> Result<PollOption, Serv
 
     Ok(option)
 }
+#[server(GetPollById)]
+pub async fn get_poll_by_id(poll_id: i32) -> Result<Poll, ServerFnError> {
+    use leptos::{server_fn::error::NoCustomError, use_context};
+    use sqlx::postgres::PgPool;
 
+    let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
+        "Unable to complete request".to_string(),
+    ))?;
+    let poll = sqlx::query_as::<_, Poll>("SELECT * FROM polls WHERE id = $1")
+        .bind(poll_id)
+        .fetch_one(&pool)
+        .await
+        .map_err(|_| {
+            ServerFnError::<NoCustomError>::ServerError("Unable to update poll".to_string())
+        })?;
+    Ok(poll)
+}
+//This is only called at the end. Do not call it for anyone but professor, but call it for everyone when exiting the eventLoop
 #[server(GetPollResults)]
 pub async fn get_poll_results(poll_id: i32) -> Result<Vec<PollOption>, ServerFnError> {
     use leptos::{server_fn::error::NoCustomError, use_context};
@@ -142,16 +285,21 @@ pub async fn get_poll_results(poll_id: i32) -> Result<Vec<PollOption>, ServerFnE
 }
 
 #[server(UpdatePoll)]
-pub async fn update_poll(poll_id: i32, question: String) -> Result<Poll, ServerFnError> {
+pub async fn update_poll(
+    poll_id: i32,
+    new_answer: String,
+    old_answer: Option<String>,
+) -> Result<Poll, ServerFnError> {
     use leptos::{server_fn::error::NoCustomError, use_context};
     use sqlx::postgres::PgPool;
+    //Check if it's a first time, or update for a poll answer
 
     let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
         "Unable to complete request".to_string(),
     ))?;
-
+    //DB is still called answer, but param is new_answer for brevity sake
     let poll: Poll = sqlx::query_as("update polls set question = $1 where id = $2 returning *")
-        .bind(question)
+        .bind(new_answer)
         .bind(poll_id)
         .fetch_one(&pool)
         .await
@@ -185,20 +333,23 @@ pub async fn delete_poll(poll_id: i32) -> Result<(), ServerFnError> {
 // Maybe make an get all polls?? May need other server functions
 
 #[server(GetAllPolls)]
-pub async fn get_all_polls() -> Result<Vec<Poll>, ServerFnError> {
+pub async fn get_all_polls(course_id: i32) -> Result<Vec<Poll>, ServerFnError> {
     use leptos::{server_fn::error::NoCustomError, use_context};
-    use sqlx::postgres::PgPool;
+    use sqlx::{postgres::PgPool, Row};
 
     let pool = use_context::<PgPool>().ok_or(ServerFnError::<NoCustomError>::ServerError(
         "Unable to complete request".to_string(),
     ))?;
 
-    let polls: Vec<Poll> = sqlx::query_as("select * from polls order by created_at desc")
-        .fetch_all(&pool)
-        .await
-        .map_err(|_| {
-            ServerFnError::<NoCustomError>::ServerError("Unable to fetch polls".to_string())
-        })?;
+    let polls: Vec<Poll> = sqlx::query_as::<_, Poll>(
+        "SELECT * FROM polls WHERE course_id = $1 ORDER BY created_at DESC",
+    )
+    .bind(course_id)
+    .fetch_all(&pool)
+    .await
+    .map_err(|_| {
+        ServerFnError::<NoCustomError>::ServerError("Unable to delete poll".to_string())
+    })?;
 
     Ok(polls)
 }
--- src/pages/live_poll.rs
+++ src/pages/live_poll.rs
@@ -1,36 +1,36 @@
+use std::time::Duration;
+
 use chrono::NaiveDateTime;
 use ev::MouseEvent;
 use leptos::*;
+use leptos::{create_effect, on_cleanup, set_interval};
 use leptos_router::use_params;
 use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::Closure;
+use wasm_bindgen::JsCast;
 
 use crate::data::database::class_functions::check_user_is_instructor;
+use crate::data::database::live_poll_functions::*;
 use crate::expect_logged_in_user;
 use crate::pages::global_components::live_poll_sidebar::Sidebar;
 use crate::pages::view_class_posts::class::ClassId;
 
-#[derive(Clone, Serialize, Deserialize, Default, Debug)]
-pub struct Poll {
-    pub id: i32,
-    pub question: String,
-    pub created_at: NaiveDateTime,
-    pub is_active: bool,
-}
-
-#[derive(Clone, Serialize, Deserialize, Default, Debug)]
-pub struct PollOption {
-    pub id: i32,
-    pub poll_id: i32,
-    pub option_text: String,
-    pub vote_count: i32,
-}
-
 #[component]
 pub fn LivePoll() -> impl IntoView {
     let (user, _) = expect_logged_in_user!();
-    let (polls, set_polls) = create_signal(Vec::<Poll>::new());
-    let (show_modal, set_show_modal) = create_signal(false);
     let class_id = use_params::<ClassId>();
+
+    let polls = create_resource(class_id, move |class_id| async move {
+        match get_all_polls(class_id.unwrap().class_id).await {
+            Ok(polls) => polls,
+            Err(err) => {
+                logging::error!("Failed to fetch polls: {}", err);
+                vec![]
+            }
+        }
+    });
+
+    let (show_modal, set_show_modal) = create_signal(false);
 
     let is_instructor = create_resource(class_id, move |class_id| {
         let user_id = user().id;
@@ -41,23 +41,20 @@ pub fn LivePoll() -> impl IntoView {
         }
     });
 
-    let create_new_poll = move |question: String| {
-        let new_poll = Poll {
-            id: polls().len() as i32 + 1,
-            question,
-            created_at: chrono::Utc::now().naive_utc(),
-            is_active: true,
-        };
-        set_polls.update(|p| p.push(new_poll));
-        set_show_modal.update(|v| *v = false);
+    let create_new_poll = move |question: String, answers: Vec<String>| {
+        spawn_local(async move {
+            let course_id = class_id().unwrap().class_id;
+            if let Ok(_) = create_poll(question, course_id, answers).await {
+                // Re-fetch the polls instead of updating manually
+                polls.refetch();
+            }
+            set_show_modal.set(false);
+        });
     };
 
     view! {
         <div class="flex">
-            // Sidebar on the left
             <Sidebar />
-
-            // Main content area for live polls
             <div class="container mx-auto my-8 p-4 flex-grow">
                 <div class="flex justify-between items-center mb-6">
                     <h1 class="text-3xl font-extrabold text-gray-800">"Live Polls"</h1>
@@ -72,117 +69,179 @@ pub fn LivePoll() -> impl IntoView {
                         </Show>
                     </Suspense>
                 </div>
-                <div class="space-y-6">
-                    {move || {
-                        polls()
-                            .iter()
-                            .map(|poll| view! { <PollCard poll=poll.clone() /> })
-                            .collect::<Vec<_>>()
-                            .into_view()
-                    }}
-                    <PollCreationModal
-                        is_visible=show_modal.into()
-                        on_close=Box::new(move |_| set_show_modal.update(|v| *v = false))
-                        on_create=Box::new(create_new_poll)
-                    />
-                </div>
+                <Suspense fallback=move || view! { <p>"Loading polls..."</p> }>
+                    {move || polls.read().map(|polls| {
+                        polls.iter().map(|poll| {
+                            view! { <PollCard poll_data=poll.clone() /> }
+                        }).collect::<Vec<_>>()
+                    }).into_view()}
+                </Suspense>
+                <PollCreationModal
+                    is_visible=show_modal.into()
+                    on_close=Box::new(move |_| set_show_modal.update(|v| *v = false))
+                    on_create=Box::new(create_new_poll)
+                />
             </div>
         </div>
     }.into_view()
 }
 
 #[component]
-pub fn PollCard(poll: Poll) -> impl IntoView {
-    let (poll_options, set_poll_options) = create_signal(vec![
-        PollOption {
-            id: 1,
-            poll_id: poll.id,
-            option_text: "Yes".to_string(),
-            vote_count: 0,
-        },
-        PollOption {
-            id: 2,
-            poll_id: poll.id,
-            option_text: "No".to_string(),
-            vote_count: 0,
-        },
-    ]);
+pub fn PollCard(poll_data: Poll) -> impl IntoView {
+    let poll = create_rw_signal(poll_data);
+    let poll_id = poll().id;
+    let (user, _) = expect_logged_in_user!();
+    let user_id = user().id;
 
-    let (selected_option, set_selected_option) = create_signal(None::<i32>);
+    let (selected_answer, set_selected_answer) = create_signal(None::<String>);
     let (has_voted, set_has_voted) = create_signal(false);
 
-    let vote_on_option = move |option_id: i32| {
-        if has_voted() {
-            return;
+    let poll_answers = create_resource(
+        move || poll_id,
+        move |poll_id| async move { get_poll_answers(poll_id).await.unwrap_or_else(|_| vec![]) },
+    );
+
+    let student_answer = create_resource(
+        move || (user_id, poll_id),
+        move |(user_id, poll_id)| async move {
+            get_student_answer(user_id, poll_id)
+                .await
+                .unwrap_or_else(|_| None)
+        },
+    );
+
+    // Update selected answer based on student's current answer
+    create_effect(move |_| {
+        if let Some(answer) = student_answer.get().flatten() {
+            set_selected_answer(Some(answer.clone()));
+            set_has_voted(true);
         }
-        set_selected_option(Some(option_id));
-        set_poll_options.update(|options| {
-            for option in options.iter_mut() {
-                if option.id == option_id {
-                    option.vote_count += 1;
-                }
+    });
+
+    // Voting function
+    let vote_on_answer = move |answer_text: String| {
+        let old_answer = selected_answer();
+        let new_answer = answer_text.clone();
+        let user_id = user().id;
+        let poll_id = poll_id;
+
+        spawn_local(async move {
+            if let Ok(_) =
+                vote_on_poll_answer(user_id, poll_id, new_answer.clone(), old_answer).await
+            {
+                set_selected_answer(Some(new_answer));
+                set_has_voted(true);
+                poll_answers.refetch();
             }
         });
-        set_has_voted(true);
     };
 
-    let options_view = move || {
-        poll_options
-            .get()
-            .iter()
-            .map(|option| {
-                let option_id = option.id;
-                let option_text = option.option_text.clone();
+    // Event loop to fetch poll results every 5 seconds
+    create_effect(move |_| {
+        if has_voted() && poll().is_active {
+            let interval_callback = Closure::wrap(Box::new(move || {
+                spawn_local(async move {
+                    // Refetch poll data to update `poll.is_active`
+                    if let Ok(updated_poll) = get_poll_by_id(poll_id).await {
+                        poll.set(updated_poll);
+                    }
+                    poll_answers.refetch();
+                });
+            }) as Box<dyn Fn()>);
 
-                view! {
-                    <button
-                        class=move || {
-                            let classes = "px-4 py-2 rounded-md w-full text-left";
-                            if selected_option() == Some(option_id) {
-                                format!("{} bg-indigo-500 text-white", classes)
-                            } else {
-                                format!("{} bg-gray-200 hover:bg-gray-300", classes)
-                            }
-                        }
-                        on:click=move |_| vote_on_option(option_id)
-                    >
-                        {option_text}
-                    </button>
-                }
-            })
-            .collect::<Vec<_>>()
-    };
+            let interval_id = window()
+                .set_interval_with_callback_and_timeout_and_arguments_0(
+                    interval_callback.as_ref().unchecked_ref(),
+                    Duration::from_secs(5).as_millis() as i32,
+                )
+                .unwrap();
+
+            // Prevent the closure from being dropped
+            interval_callback.forget();
+
+            // Cleanup interval on effect teardown
+            on_cleanup(move || {
+                window().clear_interval_with_handle(interval_id);
+            });
+        }
+    });
 
     view! {
         <div class="p-6 bg-white rounded-lg shadow-lg border border-gray-200">
-            <h2 class="mb-4 text-xl font-semibold text-gray-900">{poll.question}</h2>
-            <div class="space-y-2">{options_view}</div>
+            <h2 class="mb-4 text-xl font-semibold text-gray-900">{poll.get().question.clone()}</h2>
+            <div class="space-y-2">
+                {move || match poll_answers.get() {
+                    Some(answers) => answers.iter().map(|answer| {
+                        let answer_text = answer.answer.clone();
+                        let vote_count = answer.voted_count;
+
+                        // Clone `answer_text` for closures
+                        let answer_text_for_class = answer_text.clone();
+                        let answer_text_for_click = answer_text.clone();
+
+                        view! {
+                            <button
+                                class=move || {
+                                    let base_classes = "px-4 py-2 rounded-md w-full text-left";
+                                    if selected_answer() == Some(answer_text_for_class.clone()) {
+                                        format!("{} bg-indigo-500 text-white", base_classes)
+                                    } else {
+                                        format!("{} bg-gray-200 hover:bg-gray-300", base_classes)
+                                    }
+                                }
+                                on:click=move |_| vote_on_answer(answer_text_for_click.clone())
+                                disabled=move || !poll.get().is_active
+                            >
+                                {answer_text.clone()}
+                                {move || if !poll.get().is_active {
+                                    format!(" - {} votes", vote_count)
+                                } else {
+                                    "".to_string()
+                                }}
+                            </button>
+                        }
+                    }).collect::<Vec<_>>().into_view(),
+                    None => view! { <p>"Loading options..."</p> }.into_view(),
+                }}
+            </div>
         </div>
     }
+    .into_view()
 }
-
 #[component]
 pub fn PollCreationModal(
     is_visible: Signal<bool>,
     on_close: Box<dyn Fn(MouseEvent) + 'static>,
-    on_create: Box<dyn Fn(String) + 'static>,
+    on_create: Box<dyn Fn(String, Vec<String>) + 'static>,
 ) -> impl IntoView {
     let (question, set_question) = create_signal(String::new());
+    let (answers, set_answers) = create_signal(vec![String::new()]);
+
+    // Function to add a new answer field
+    let add_answer_field = move |_| {
+        set_answers.update(|ans| {
+            ans.push(String::new());
+        });
+    };
 
     let create_poll = move |_| {
-        if !question().is_empty() {
-            on_create(question());
+        let question_text = question();
+        let answers_list = answers()
+            .iter()
+            .filter(|a| !a.trim().is_empty())
+            .cloned()
+            .collect::<Vec<_>>();
+        if !question_text.is_empty() && !answers_list.is_empty() {
+            on_create(question_text, answers_list);
             set_question.set(String::new());
+            set_answers.set(vec![String::new()]);
         }
     };
 
     view! {
-        // Modal Background
         <div class=move || {
             if is_visible() { "fixed z-40 inset-0 bg-black bg-opacity-50" } else { "hidden" }
         }></div>
-
-        // Modal Content
         <div class=move || {
             if is_visible() { "fixed z-50 inset-0 flex items-center justify-center" } else { "hidden" }
         }>
@@ -197,7 +256,31 @@ pub fn PollCreationModal(
                         set_question.set(event_target_value(&ev));
                     }
                 />
-                <div class="flex justify-end space-x-4">
+      {move || {
+        let answers_vec = answers().clone();
+        answers_vec.into_iter().enumerate().map(move |(index, answer)| {
+            view! {
+                <input
+                    type="text"
+                    class="w-full rounded-lg border-gray-300 shadow-sm p-2 mb-2 focus:border-indigo-500 focus:ring-indigo-500"
+                    placeholder=format!("Answer option {}", index + 1)
+                    value=answer
+                    on:input=move |ev| {
+                        let value = event_target_value(&ev);
+                        set_answers.update(move |ans| {
+                            let mut new_answers = ans.clone();
+                            new_answers[index] = value;
+                            *ans = new_answers;
+                        });
+                    }
+                />
+            }
+        }).collect::<Vec<_>>()
+    }}
+                <button on:click=add_answer_field class="py-2 px-4 bg-gray-300 rounded-lg hover:bg-gray-400">
+                    "+ Add Answer Option"
+                </button>
+                <div class="flex justify-end space-x-4 mt-4">
                     <button
                         class="py-2 px-4 bg-gray-300 rounded-lg hover:bg-gray-400"
                         on:click=on_close
@@ -205,9 +288,9 @@ pub fn PollCreationModal(
                         "Cancel"
                     </button>
                     <button
-                    class="py-2 px-4 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded hover:from-blue-600 hover:to-indigo-700"
-                    on:click=create_poll
-                        prop:disabled=move || question().trim().is_empty()
+                        class="py-2 px-4 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded hover:from-blue-600 hover:to-indigo-700"
+                        on:click=create_poll
+                        prop:disabled=move || question().trim().is_empty() || answers().iter().all(|a| a.trim().is_empty())
                     >
                         "+ Create"
                     </button>
