56be3e116ea4348d755dd41d542a9e755eb1c0b3
gabrielfamodu <gabefamodu13@gmail.com>
2024-11-25 00:13:45 -0700

    add textured background with icons

--- src/app.rs
+++ src/app.rs
@@ -204,7 +204,11 @@ pub fn AuthenticatedRoutes() -> impl IntoView {
 /// General route wrapping
 #[component]
 fn UnauthenticatedRoutes() -> impl IntoView {
-    view! { <Outlet /> }
+    view! {
+      <div class="w-auto h-auto bg-cover bg-[url('/images/pattern_bg.png')]">
+        <Outlet />
+      </div>
+    }
 }
 
 // [Application-wide helpers] ===============================================
