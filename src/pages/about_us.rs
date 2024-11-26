db4608016f77abaf4386e70bbda8169d38a9d14e
Matthew <m4ttm4n.t4yl0r@gmail.com>
2024-11-25 21:34:24 -0700

    Updated description

--- src/pages/about_us.rs
+++ src/pages/about_us.rs
@@ -8,10 +8,10 @@ pub fn AboutUs() -> impl IntoView {
           <header class="flex justify-between items-center py-4">
             <h1 class="text-4xl font-bold">"About Us"</h1>
           </header>
-          <main class="my-8 text-center">
+          <main class="my-8 text-left">
             <h1 class="mb-4 text-xl">"Project Overview:"</h1>
             <p class="mb-4 text-xl">
-              "Encampus is a classroom assistance tool with the purpose of streamlining communication within classes, enabling students to ask both public and private questions outside of regular hours. Additionally, Encampus acts as a forum for students to help share insights and answer each other's questions. Another option for in-person meetings, catering to the busy schedules of diverse students on college campuses and making getting help on assignments convenient. Encampus distinguishes itself by building upon and refining the core functionalities found in similar software offering familiar tools such as Q&A forums, student participation tracking, AI responses, and live notifications. "
+              "Encampus is a classroom assistance tool with the purpose of streamlining communication within classes, enabling students to ask both public and private questions outside of regular hours. Additionally, Encampus acts as a forum for students to help share insights and answer each other's questions. Another option for in-person meetings, catering to the busy schedules of diverse students on college campuses and making getting help on assignments convenient. Encampus distinguishes itself by building upon and refining the core functionalities found in similar software offering familiar tools such as Q&A forums, student participation tracking, AI responses, and live notifications."
             </p>
             <h1 class="mb-4 text-xl">"Team Members:"</h1>
             <div class="grid grid-cols-2 gap-4">
