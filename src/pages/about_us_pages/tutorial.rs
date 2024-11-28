320d1c43b78ba7f625644faa163a7684c3040887
Wensen Zhang <wensen.zhg@gmail.com>
2024-11-27 23:29:14 -0700

    Add tutorial page

--- src/pages/about_us_pages/tutorial.rs
+++ src/pages/about_us_pages/tutorial.rs
@@ -2,5 +2,15 @@ use leptos::{component, view, IntoView};
 
 #[component]
 pub fn Tutorial() -> impl IntoView {
-    view! {}
+    view! {
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/ZAPuSBolj1U"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    }
 }
