199fc389612f57f08bd0e58f59b65debaef7570f
Wensen Zhang <wensen.zhg@gmail.com>
2024-11-27 23:35:15 -0700

    Adding minor style updates

--- src/pages/about_us_pages/tutorial.rs
+++ src/pages/about_us_pages/tutorial.rs
@@ -3,14 +3,19 @@ use leptos::{component, view, IntoView};
 #[component]
 pub fn Tutorial() -> impl IntoView {
     view! {
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/ZAPuSBolj1U"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+      <div class="flex justify-center items-center min-h-screen bg-gray-100">
+        <div class="p-6 bg-white rounded-lg shadow-lg">
+          <iframe
+            class="rounded-md"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/ZAPuSBolj1U"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
     }
 }
