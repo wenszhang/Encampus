c7e143f1a34d07932d0e0cc123a0d9cc4d67acc5
gabrielfamodu <gabefamodu13@gmail.com>
2024-12-06 11:02:42 -0700

    reused header and sidebar components in live poll feature

--- src/pages/global_components/mod.rs
+++ src/pages/global_components/mod.rs
@@ -3,7 +3,6 @@ pub mod alerts;
 pub mod announcements;
 pub mod error_template;
 pub mod header;
-pub mod live_poll_sidebar;
 pub(crate) mod notification;
 pub mod page;
 pub mod push_notifications;
